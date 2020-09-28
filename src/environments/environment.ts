1dfb6280ce4dce2947cafc6b60d010e6b7e28f65
Hedgehuug <47126622+Hedgehuug@users.noreply.github.com>
2020-09-28 14:05:24 +0200

    gitignore update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,15 @@
 
 export const environment = {
   production: false,
-  //Information has been removed from this file for the preservation of privacy
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyC9yN-Jty8Jdy5QPDqj6QRtfN96ErN-CwA",
+    authDomain: "turtlejournal-69ce1.firebaseapp.com",
+    databaseURL: "https://turtlejournal-69ce1.firebaseio.com",
+    projectId: "turtlejournal-69ce1",
+    storageBucket: "turtlejournal-69ce1.appspot.com",
+    messagingSenderId: "665517918734",
+    appId: "1:665517918734:web:ceb0ebca1d343c5b200a5f",
+    measurementId: "G-HXDWCCZYTM"
   }
 };
 
